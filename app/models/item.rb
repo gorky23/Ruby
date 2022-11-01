707cc3edf07362fbfcbf30aa2e7ede0b7be1cac7
Tomek <gorky23@o2.pl>
2022-11-01 22:46:53 +0100

    Scopes

--- app/models/item.rb
+++ app/models/item.rb
@@ -14,4 +14,9 @@ class Item < ApplicationRecord
   validates :name, presence: true, length: { minimum: 5 }
 
   enum item_type: %i[ book cd electric_device flower other ]
+
+  scope :active, -> { where(active: true) }
+  scope :inactive, -> { where(active: false) }
+  scope :borrowed, -> { where(borrowed: true) }
+  scope :on_place, -> { where(borrowed: false) }
 end
