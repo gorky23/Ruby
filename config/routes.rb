a4bccd63a3d8784f541b292970b6c06a124e6d96
Tomek <gorky23@o2.pl>
2022-11-01 22:38:53 +0100

    New View and filter

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,12 @@
 
 Rails.application.routes.draw do
   root 'items#index'
-  resources :items
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :items do
+    collection do
+      get :active
+      get :inactive
+      get :borrowed
+      get :on_place
+    end
+  end
 end
