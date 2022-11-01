30375bb9abb175585a4663be027fc7877daf560a
Tomek <gorky23@o2.pl>
2022-11-01 21:55:41 +0100

    Validation, add column and enum

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,9 +5,13 @@
 #  id         :bigint           not null, primary key
 #  active     :boolean
 #  borrowed   :boolean
+#  item_type  :integer          default("other"), not null
 #  name       :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
 class Item < ApplicationRecord
+  validates :name, presence: true, length: { minimum: 5 }
+
+  enum item_type: %i[ book cd electric_device flower other ]
 end
