e4e68e0a2b5f765c3b48296a9bc7d281941c1448
Tomek <gorky23@o2.pl>
2022-11-08 21:51:21 +0100

    Add validates for all(name, surname, email)

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,4 +1,10 @@
 class Friend < ApplicationRecord
   validates :name, length: { minimum: 5 }
   validates :name, presence: true
+
+  validates :surname, length: { minimum: 5 }
+  validates :surname, presence: true
+
+  validates :email, length: { minimum: 5 }
+  validates :email, presence: true
 end
