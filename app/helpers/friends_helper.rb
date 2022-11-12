e721b86aaffcadf0b64ef181bd238f1c1e7d6bae
Tomek <gorky23@o2.pl>
2022-11-12 02:38:11 +0100

    Add partials

--- app/helpers/friends_helper.rb
+++ app/helpers/friends_helper.rb
@@ -1,4 +1,10 @@
 module FriendsHelper
+  def friends()
+    return "APLIKACJA FRIENDS"
+  end
+  def nav_link_class(path)
+    [ 'nav-link', ('active' if current_page?(path)) ]
+  end
   def dropdown_link_class(path)
     [ 'dropdown-item', ('active' if current_page?(path)) ]
   end
