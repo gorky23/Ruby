f8b2722c3f544d0c76fd09118585b75bdc175798
Tomek <gorky23@o2.pl>
2022-11-16 23:36:16 +0100

    Add method_spec.rb

---
+++ spec/method_spec.rb
@@ -0,0 +1,13 @@
+def full_name(first_name, last_name)
+  "#{first_name} #{last_name}"
+end
+
+describe 'test method' do
+  it 'returns full name' do
+    expect(full_name("Tomasz", "Dziurman")).to eq "Tomasz Dziurman"
+  end
+
+  it 'returns full name based on params' do
+    expect(full_name('John', 'Doe')).to eq 'John Doe'
+  end
+end
