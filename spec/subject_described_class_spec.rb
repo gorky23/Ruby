59a6c17fd81badfa03170dd5bd601f176cb84937
Tomek <gorky23@o2.pl>
2022-11-20 00:18:21 +0100

    Add let, let! and before inRSpec

--- spec/subject_described_class_spec.rb
+++ spec/subject_described_class_spec.rb
@@ -9,22 +9,42 @@ class MyLogger
     @messages << message
     message
   end
+
+  def messages_count
+    @messages.count
+  end
 end
 
 describe MyLogger do
   subject(:logger) { described_class.new }
+  let(:message1) { 'Hello World' }
+  let(:message2) { 'Ruby is awesome!' }
+
+  before do
+    p "run before"
+  end
+
   it 'logs a message' do
-    logged_message = logger.log("Hello World")
+    logged_message = logger.log(message1)
     expect(logged_message).to include 'Hello World'
   end
 
-  it 'adds message to messages array' do
-    message1 = 'Hello World'
-    message2 = 'Ruby is Awesome!'
-    logger.log(message1)
-    logger.log(message2)
-    expect(logger.messages).to include message1
-    expect(logger.messages).to include message2
+  context 'for logging' do
+    let(:message1) { 'Other message' }
 
+    before do
+      logger.log(message1)
+      logger.log(message2)
+    end
+
+    it 'adds message to messages array' do
+      expect(logger.messages).to include message1
+      expect(logger.messages).to include message2
+    end
+
+    it 'returns messages count' do
+      expect(logger.messages_count).to eq 2
+    end
   end
+
 end
