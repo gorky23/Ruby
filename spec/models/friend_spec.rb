bba4cd912618b6d85c3c06c8e34fd3dd6017a5ce
Tomek <gorky23@o2.pl>
2022-12-04 16:24:42 +0100

    Add test for friend db columns - name if exist

---
+++ spec/models/friend_spec.rb
@@ -0,0 +1,10 @@
+describe Friend, type: :model do
+  describe 'columns' do
+    it { is_expected.to have_db_column(:name).of_type(:string) }
+    it { is_expected.to have_db_column(:surname).of_type(:string) }
+    it { is_expected.to have_db_column(:email).of_type(:string) }
+    it { is_expected.to have_db_column(:place).of_type(:integer) }
+    it { is_expected.to have_db_column(:created_at).of_type(:datetime) }
+    it { is_expected.to have_db_column(:updated_at).of_type(:datetime) }
+  end
+end
