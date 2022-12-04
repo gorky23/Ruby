6d11346c434e50d82306ec9e6b0b89e20b2a344e
Tomek <gorky23@o2.pl>
2022-12-04 16:41:36 +0100

    Add test for friend model validations

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -9,6 +9,7 @@ class Friend < ApplicationRecord
   validates :email, presence: true
 
   enum place: %i[ school work party vacation shop gym restaurant gallery other ]
+  validates :place, inclusion: { in: places.keys }
 
   scope :school, -> { where(place: :school) }
   scope :work, -> { where(place: :work) }
--- spec/models/friend_spec.rb
+++ spec/models/friend_spec.rb
@@ -7,4 +7,14 @@ describe Friend, type: :model do
     it { is_expected.to have_db_column(:created_at).of_type(:datetime) }
     it { is_expected.to have_db_column(:updated_at).of_type(:datetime) }
   end
+
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:surname) }
+    it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to define_enum_for(:place).with_values(Friend.places) }
+    it { is_expected.to validate_length_of(:name).is_at_least(3) }
+    it { is_expected.to validate_length_of(:surname).is_at_least(5) }
+    it { is_expected.to validate_length_of(:email).is_at_least(5) }
+  end
 end
