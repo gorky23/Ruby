d3a16639833b63402c9831cf49b7e68e5dc84079
Tomek <gorky23@o2.pl>
2022-12-04 15:52:44 +0100

    Add shoulda_matchers configuration to rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -16,4 +16,11 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
+
+  Shoulda::Matchers.configure do |config|
+    config.integrate do |with|
+      with.test_framework :rspec
+      with.library :rails
+    end
+  end
 end
