d3cbe40d78fc91687137efdec4480f4598394c9b
Tomek <gorky23@o2.pl>
2022-12-14 00:05:20 +0100

    Safety data, current_user, repair data in console and with rake task

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,7 +6,7 @@ class ItemsController < ApplicationController
   layout 'application'
   # GET /items or /items.json
   def index
-    @items = Item.all
+    @items = current_user.items
   end
 
   # GET /items/1 or /items/1.json
@@ -14,7 +14,7 @@ class ItemsController < ApplicationController
 
   # GET /items/new
   def new
-    @item = Item.new
+    @item = current_user.items.build
   end
 
   # GET /items/1/edit
@@ -22,7 +22,7 @@ class ItemsController < ApplicationController
 
   # POST /items or /items.json
   def create
-    @item = Item.new(item_params)
+    @item = current_user.items.new(item_params)
 
     respond_to do |format|
       if @item.save
@@ -59,26 +59,26 @@ class ItemsController < ApplicationController
   end
 
   def active
-    @items = Item.active
+    @items = current_user.items.active
   end
 
   def inactive
-    @items = Item.inactive
+    @items = current_user.items.inactive
   end
 
   def borrowed
-    @items = Item.borrowed
+    @items = current_user.items.borrowed
   end
 
   def on_place
-    @items = Item.on_place
+    @items = current_user.items.on_place
   end
 
   private
 
   # Use callbacks to share common setup or constraints between actions.
   def set_item
-    @item = Item.find(params[:id])
+    @item = current_user.items.find(params[:id])
   end
 
   # Only allow a list of trusted parameters through.
---
+++ lib/tasks/items_manager.rb
@@ -0,0 +1,3 @@
+class ItemsManager
+
+end
