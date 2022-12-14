104d28e53e76341b0763429c5cbf559a945aec84
Tomek <gorky23@o2.pl>
2022-12-15 00:53:55 +0100

    Add model Friendshiprelation

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,5 +28,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable
+
   has_many :items, dependent: :destroy
+  has_many :friendships, inverse_of: :user, dependent: :destroy
+  has_many :friends, through: :friendships
 end
